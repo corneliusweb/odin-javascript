40a7a3957a5e54793f529d27530116b1bc22c4f7
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-12 12:33:58 +0100

    Complete FizzBuzz exercize for Algorithm practice

--- odin/lessons.js
+++ odin/lessons.js
@@ -14,16 +14,68 @@ console.log(multiply(67, 3));
  * Write a function called capitalize that takes a string and returns that string with only the first letter capitalized. Make sure that it can take strings that are lowercase, UPPERCASE or BoTh.
  */
 function capitalize(str) {
+	// Convert all the strings to lowercase
 	const lowercase = str.toLowerCase();
+	// Get the first letter of the lowercased strings by index and store in a variable
 	const firstLetter = lowercase[0];
+	// Replace the first letter of the lowercased string by referencing its index, and call the toUpperCase function
 	const converted = lowercase.replace(firstLetter, firstLetter.toUpperCase());
-
+	// Return the result of the processes.
 	return converted;
 }
-console.log(capitalize('hello'));
+console.log(capitalize('hELlo'));
 
 // Write a function called lastLetter that takes a string and returns the very last letter of that string:
 function lastLetter(str) {
 	return str[str.length - 1];
 }
-console.log(lastLetter('jammy'))
+console.log(lastLetter('jammy'));
+
+// Practicing Algorithm with FizBuzz game
+
+/**
+ * Get and store the user input in a variable
+ *    In this example, I'll use function
+ * Convert the input to number
+ * Create a loop to run from the user input till 0
+ * Check if the number is divisible by 3 and 5
+ * If it is, log 'FizBuzz'
+ * Check if the number is divisible by 3 or 5
+ * If it is, log Fizz or Buzz respectively
+ * Else, log the number
+ */
+
+// Very unnecessary, but I have got practice function parameters
+function getInput(input) {
+	// input = +prompt('Enter number');
+
+	// If I am getting the input from DOM, test if it's a number
+	if (isNaN(input)) {
+		return; // returns undefined if not a number
+	} else {
+		return input;
+	}
+}
+
+function checkFizzBuzz(num) {
+	// First check if the getInput function returned `undefined`
+   if (typeof num !== 'undefined') {
+      
+		//Create a loop to run from the user input till 0
+		for (let c = 1; c <= num; c++) {
+			if (c % 3 === 0 && c % 5 === 0) {
+				console.log('FizBuzz');
+			} else if (c % 3 === 0) {
+				console.log('Fizz');
+			} else if (c % 5 === 0) {
+				console.log('Buzz');
+			} else {
+				console.log(c);
+			}
+		}
+		// if getInput function returned `undefined`, log this
+	} else {
+		console.log("Hey man! Can't you read?");
+	}
+}
+checkFizzBuzz(getInput(+'8' + +'8'));
