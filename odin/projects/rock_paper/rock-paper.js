ad24ae74a6c70e54a787d232fb74a74647fe50f1
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-14 07:53:20 +0100

    Create playGame function to handle playRound calls

--- odin/projects/rock_paper/rock-paper.js
+++ odin/projects/rock_paper/rock-paper.js
@@ -13,7 +13,8 @@ function getComputerChoice() {
 // console.log(getComputerChoice());
 
 function getHumanChoice() {
-	const userChoice = prompt('Rock, Paper, Scissors?');
+   const userChoice = prompt('Rock, Paper, Scissors?');
+   // check if a user entered a valid character to avoid throwing error for trying to convert empty strings.
 	if (userChoice) {
 		const lowercase = userChoice.toLowerCase();
 		const capitalized = lowercase.replace(
@@ -25,40 +26,46 @@ function getHumanChoice() {
 }
 // console.log(getHumanChoice());
 
-let humanChoice = 0;
-let computerChoice = 0;
+function playGame() {
+	let humanScore = 0;
+	let computerScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-	if (computerChoice === humanChoice) {
-		console.log(`That's a tie. Go again!`);
+   function playRound(humanChoice, computerChoice) {
+      // check if computer and the user chose the same word
+		if (computerChoice === humanChoice) {
+			console.log(`That's a tie. Go again!`);
+		}
+
+		if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
+			console.log(`You lose! Rock breaks scissors`);
+			computerScore++;
+		} else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
+			console.log(`You win! Paper traps rock`);
+			humanScore++;
+		}
+
+		if (computerChoice === 'Paper' && humanChoice === 'Rock') {
+			console.log(`You lose! Paper traps rock`);
+			computerScore++;
+		} else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
+			console.log(`You win! Scissors cuts paper`);
+			humanScore++;
+		}
+
+		if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
+			console.log(`You lose! Scissor cuts paper`);
+			computerScore++;
+		} else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
+			console.log(`You win! Rock breaks scissors`);
+			humanScore++;
+		}
 	}
 
-	if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
-		console.log(`You lose! Rock breaks scissors`);
-		computerChoice++;
-	} else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
-		console.log(`You win! Paper traps rock`);
-		humanChoice++;
-	}
+	const roundCount = 5;
 
-	if (computerChoice === 'Paper' && humanChoice === 'Rock') {
-		console.log(`You lose! Paper traps rock`);
-		computerChoice++;
-	} else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
-		console.log(`You win! Scissors cuts paper`);
-		humanChoice++;
+	for (let i = 1; i <= roundCount; i++) {
+		playRound(getHumanChoice(), getComputerChoice());
 	}
-
-	if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
-		console.log(`You lose! Scissor cuts paper`);
-		computerChoice++;
-	} else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
-		console.log(`You win! Rock breaks scissors`);
-		humanChoice++;
-	}
+	console.log(`Computer score: ${computerScore}\nYour score: ${humanScore}`);
 }
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
