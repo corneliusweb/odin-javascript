53fd0e7f981ad5c68518038845c384d904cdb0c1
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-14 07:07:06 +0100

    Create play round function and set game rules with conditionals

--- odin/projects/rock_paper/rock-paper.js
+++ odin/projects/rock_paper/rock-paper.js
@@ -10,17 +10,55 @@ function getComputerChoice() {
 		return 'Scissors';
 	}
 }
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 function getHumanChoice() {
 	const userChoice = prompt('Rock, Paper, Scissors?');
-   if (userChoice) {
-      const lowercase = userChoice.toLowerCase();
+	if (userChoice) {
+		const lowercase = userChoice.toLowerCase();
 		const capitalized = lowercase.replace(
 			lowercase[0],
 			lowercase[0].toUpperCase()
 		);
 		return capitalized;
-   }
+	}
 }
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+let humanChoice = 0;
+let computerChoice = 0;
+
+function playRound(humanChoice, computerChoice) {
+	if (computerChoice === humanChoice) {
+		console.log(`That's a tie. Go again!`);
+	}
+
+	if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
+		console.log(`You lose! Rock breaks scissors`);
+		computerChoice++;
+	} else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
+		console.log(`You win! Paper traps rock`);
+		humanChoice++;
+	}
+
+	if (computerChoice === 'Paper' && humanChoice === 'Rock') {
+		console.log(`You lose! Paper traps rock`);
+		computerChoice++;
+	} else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
+		console.log(`You win! Scissors cuts paper`);
+		humanChoice++;
+	}
+
+	if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
+		console.log(`You lose! Scissor cuts paper`);
+		computerChoice++;
+	} else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
+		console.log(`You win! Rock breaks scissors`);
+		humanChoice++;
+	}
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
