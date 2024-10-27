eecc1aefedaaf7f4670fbb3738d035a028b22707
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-27 02:55:37 +0100

    Add UI and game state notifications

--- odin/projects/rock_paper/rock-paper.js
+++ odin/projects/rock_paper/rock-paper.js
@@ -12,60 +12,70 @@ function getComputerChoice() {
 }
 // console.log(getComputerChoice());
 
-function getHumanChoice() {
-   const userChoice = prompt('Rock, Paper, Scissors?');
-   // check if a user entered a valid character to avoid throwing error for trying to convert empty strings.
-	if (userChoice) {
-		const lowercase = userChoice.toLowerCase();
-		const capitalized = lowercase.replace(
-			lowercase[0],
-			lowercase[0].toUpperCase()
-		);
-		return capitalized;
-	}
-}
-// console.log(getHumanChoice());
-
 function playGame() {
 	let humanScore = 0;
 	let computerScore = 0;
 
-   function playRound(humanChoice, computerChoice) {
-      // check if computer and the user chose the same word
+	const roundResult = document.querySelector('.round-result');
+	const runningScore = document.querySelector('.running-score');
+	const finalResult = document.querySelector('.final-result');
+
+	function playRound(humanChoice, computerChoice) {
+		// check if computer and the user chose the same word
 		if (computerChoice === humanChoice) {
-			console.log(`That's a tie. Go again!`);
+			roundResult.textContent = `That's a tie. Go again!`;
 		}
 
 		if (computerChoice === 'Rock' && humanChoice === 'Scissors') {
-			console.log(`You lose! Rock breaks scissors`);
 			computerScore++;
+			roundResult.textContent = `You lose! Rock breaks scissors`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
 		} else if (computerChoice === 'Rock' && humanChoice === 'Paper') {
-			console.log(`You win! Paper traps rock`);
 			humanScore++;
+			roundResult.textContent = `You win! Paper traps rock`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
 		}
 
 		if (computerChoice === 'Paper' && humanChoice === 'Rock') {
-			console.log(`You lose! Paper traps rock`);
 			computerScore++;
+			roundResult.textContent = `You lose! Paper traps rock`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
 		} else if (computerChoice === 'Paper' && humanChoice === 'Scissors') {
-			console.log(`You win! Scissors cuts paper`);
 			humanScore++;
+			roundResult.textContent = `You win! Scissors cuts paper`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
 		}
 
 		if (computerChoice === 'Scissors' && humanChoice === 'Paper') {
-			console.log(`You lose! Scissor cuts paper`);
 			computerScore++;
+			roundResult.textContent = `You lose! Scissor cuts paper`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
 		} else if (computerChoice === 'Scissors' && humanChoice === 'Rock') {
-			console.log(`You win! Rock breaks scissors`);
 			humanScore++;
+			roundResult.textContent = `You win! Rock breaks scissors`;
+			runningScore.textContent = `Your Score: ${humanScore}. Computer Score: ${computerScore}`;
+      }
+      
+		if (humanScore === 5 && computerScore < 5) {
+			finalResult.textContent = 'You win!!';
+		} else if (computerScore === 5 && humanScore < 5) {
+			finalResult.textContent = 'Computer win!!!';
 		}
 	}
 
-	const roundCount = 5;
-
-	for (let i = 1; i <= roundCount; i++) {
-		playRound(getHumanChoice(), getComputerChoice());
-	}
-	console.log(`Computer score: ${computerScore}\nYour score: ${humanScore}`);
+	const btns = document.querySelector('.game-btns');
+	btns.addEventListener('click', (event) => {
+		let playerSelection = '';
+		if (event.target.classList.contains('rock')) {
+			playerSelection = 'Rock';
+			playRound(playerSelection, getComputerChoice());
+		} else if (event.target.classList.contains('paper')) {
+			playerSelection = 'Paper';
+			playRound(playerSelection, getComputerChoice());
+		} else if (event.target.classList.contains('scissors')) {
+			playerSelection = 'Scissors';
+			playRound(playerSelection, getComputerChoice());
+		}
+	});
 }
 playGame();
