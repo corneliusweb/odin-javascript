f463054c7cb8e697dba5530f72285233a424128a
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-27 13:56:07 +0100

    Add currentGrid to hold grids per generation

--- odin/projects/etch-a-sketch/app.js
+++ odin/projects/etch-a-sketch/app.js
@@ -1,17 +1,21 @@
 const container = document.querySelector('.container');
 
-function generateGrid(num) {
+function renderGrid(num) {
 	const gridNum = num * num;
+
+   const currentGrid = document.createElement('div');
+   currentGrid.classList.add('current-grid')
+	container.appendChild(currentGrid);
 
 	let i = 1;
 	while (i <= gridNum) {
 		const grid = document.createElement('div');
-		const gridWidth = Number((97 / num).toFixed(2)); // dividing by 95% instead of 100% makes sure that the gapping is accounted for.
+		const gridWidth = Number((95 / num).toFixed(2)); // dividing by 95% instead of 100% makes sure that the gapping is accounted for.
 		console.log(gridWidth);
 		grid.style.flex = `1 1 ${gridWidth}%`;
 		grid.style.aspectRatio = '1/1';
 		grid.style.backgroundColor = '#222';
-		container.appendChild(grid);
+		currentGrid.appendChild(grid);
 
 		i++;
 	}
@@ -22,9 +26,9 @@ const button = document.querySelector('button');
 button.addEventListener('click', () => {
 	const num = +prompt('Enter grid size');
 	if (!num) alert('Enter a valid number');
-   if (num > 100) alert('Enter numbers less than or equal to 100');
-   
+	if (num > 100) alert('Enter a number less than or equal to 100');
+
 	if (num <= 100) {
-		generateGrid(num);
+		renderGrid(num);
 	}
 });
