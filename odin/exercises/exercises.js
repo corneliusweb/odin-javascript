ce0c351283dcdab07e2559ffa494d5d839104dfa
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-10-28 11:19:09 +0100

    Write the static solution to test my knowledge

--- odin/exercises/exercises.js
+++ odin/exercises/exercises.js
@@ -40,3 +40,33 @@ divPara.textContent = 'ME TOO!';
 div.appendChild(divPara);
 
 body.appendChild(div);
+
+
+/** function sumOfTripledEvens(array) {
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		// Step 1: If the element is an even number
+		if (array[i] % 2 === 0) {
+			// Step 2: Multiply this number by three
+			const tripleEvenNumber = array[i] * 3;
+
+			// Step 3: Add the new number to the total
+			sum += tripleEvenNumber;
+		}
+	}
+	return sum;
+}
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// first, filter even numbers
+// second, map to times by 3
+// lastly, reduce to sum all
+
+let filteredArr = arr.filter((num) => num % 2 === 0);
+let mappedArray = filteredArr.map((num) => num * 3);
+let reducedArr = mappedArray.reduce((total, num) => {
+   return total + num;
+}, 0)
+console.log(reducedArr)
