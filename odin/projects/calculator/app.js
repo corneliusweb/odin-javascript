f12e448ff136ccf62a1c43519469a645ffb7a496
Cornelius Asogwa <cornelius.webdev@gmail.com>
2024-11-01 20:23:18 +0100

    Get all the numeric and function buttons, and prevent form default

--- odin/projects/calculator/app.js
+++ odin/projects/calculator/app.js
@@ -17,3 +17,46 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
 	return num1 / num2;
 }
+
+document
+	.querySelector('form')
+	.addEventListener('click', (event) => event.preventDefault());
+
+const funcAndNumericBtns = document.querySelector('.func-and-numeric-btns');
+
+funcAndNumericBtns.addEventListener('click', (event) => {
+	if (event.target.classList.contains('remainder')) {
+		console.log('%');
+	} else if (event.target.classList.contains('negation')) {
+		console.log('-/+');
+	} else if (event.target.classList.contains('clear')) {
+		console.log('C');
+   }
+   
+   // numeric buttons
+   if (event.target.classList.contains('one')) {
+		console.log('1');
+	} else if (event.target.classList.contains('two')) {
+		console.log('2');
+	} else if (event.target.classList.contains('three')) {
+		console.log('3');
+	} else if (event.target.classList.contains('four')) {
+		console.log('4');
+	} else if (event.target.classList.contains('five')) {
+		console.log('5');
+	} else if (event.target.classList.contains('six')) {
+		console.log('6');
+	} else if (event.target.classList.contains('seven')) {
+		console.log('7');
+	} else if (event.target.classList.contains('eight')) {
+		console.log('8');
+	} else if (event.target.classList.contains('nine')) {
+		console.log('9');
+   } else if (event.target.classList.contains('zero')) {
+      console.log('0')
+   }
+   
+   if (event.target.classList.contains('dot')) {
+      console.log('dot')
+   }
+});
